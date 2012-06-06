cf35ef9d287ea1d51d303e30ef4c995b4398a37a
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-06 13:07:35 +0800

    removed the link to gift packaging config from top menu

--- app/overrides/add_gift_packages_to_admin.rb
+++ app/overrides/add_gift_packages_to_admin.rb
@@ -8,15 +8,12 @@ Deface::Override.new(:virtual_path => 'spree/admin/configurations/index',
                         </tr> },
                       :disabled => false)
                       
-                      
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "retailers_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:gift_packages, :url => 'spree.admin_retailers_path') %>",
-                     :disabled => false)
 
 Deface::Override.new(:virtual_path => 'spree/admin/shipments/_form',
                       :name => 'add_gift_packaging_to_shipment_form',
                       :insert_before => "[data-hook='admin_shipment_form_address']",
                       :partial => 'spree/admin/shipments/gift_packaging',
                       :diabled => false)
+                      
+                      
+                                            
