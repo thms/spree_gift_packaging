ac25a5a0e5978a444cc5c574ace19c6f5e205566
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-15 17:35:27 +0800

    Missed one file

---
+++ app/models/spree/current_order_decorator.rb
@@ -0,0 +1,13 @@
+Spree::Core::CurrentOrder.module_eval do
+
+  # This overrides the method as defined in spree_auth, so that we can create the gift_packaging_charge here
+  def after_save_new_order
+    # create the initial gift packaging charge for the order
+    @current_order.create_gift_packaging_charge!
+    
+    # make sure the user has permission to access the order (if they are a guest)
+    return if current_user
+    session[:access_token] = @current_order.token
+  end
+  
+end
