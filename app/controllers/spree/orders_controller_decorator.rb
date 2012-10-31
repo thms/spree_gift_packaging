8d165f3d50e0b6e556a1e189b1808ff8b99ae726
Thomas Boltze <thomas.boltze@gmail.com>
2012-10-31 10:25:04 +0800

    Added abiility to add gift package after the 'add to cart from tile'

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,5 +1,9 @@
 Spree::OrdersController.class_eval do
 
+  respond_to :js, :only => [:populate, :update, :change_gift_package]
+
+  ssl_allowed :populate, :update, :change_gift_package
+
   # Override method to allow adding of gift packaging from the various cart forms
   
   # Adds a new item to the order (creating a new order if none already exists)
@@ -42,10 +46,25 @@ Spree::OrdersController.class_eval do
       end
       line_item = @order.add_variant_with_gift_package(@variant, quantity, gift_package_id) if quantity > 0
     end if params[:variants]
-
+    
+    # get the source of add to cart - if we come from a tile, we want to show the gift packging selector, if we come from a product, we do not want to show it
+    @source = params[:source]
     fire_event('spree.cart.add')
     fire_event('spree.order.contents_changed')
-    respond_with(@order) { |format| format.html { redirect_to cart_path } }
+    respond_with(@order, @source) { |format| format.html { redirect_to cart_path } }
   end
+  
+  # allow setting the gift packaging for an item in the cart
+  # get the product id and the gift package id, looks up the line item and sets the giftpackage for that.
+  def change_gift_package
+    @order = current_order(false)
+    @product = Spree::Product.find(params[:product_id])
+    @gift_package = Spree::GiftPackage.find(params[:gift_package_id])
+    line_item = @order.line_items.where(:variant_id => @product.master.id).first
+    line_item.update_attribute(:gift_package_id, @gift_package.id)
+    fire_event('spree.order.contents_changed')
+    respond_with(@order)
+  end
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Spree::Core::Engine.routes.prepend do
   
+  match '/orders/change_gift_package', :to => 'orders#change_gift_package', :via => :post
+  
   namespace :admin do
     
     resources :gift_packages
